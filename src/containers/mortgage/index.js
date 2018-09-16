1e1ad3824973e0c62a9d0a5792e8e0cd1cb24b60
Chris Lorenzo <Christopher_Lorenzo@Comcast.com>
2018-09-16 08:34:03 -0400

    Add mortgage calculator code

--- src/containers/mortgage/index.js
+++ src/containers/mortgage/index.js
@@ -1,21 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  input: {
+    display: 'none'
+  }
+});
+
+/*
+  Todo (HW):
+  1. Why is it not setting initial payment value
+  2. Style to make input boxes vertical
+  3. Fix payment field label and be readonly.
+
+  Understand how React sets default state
+
+  Next Week
+
+  Add Calculate Button to update payment
+
+  Lay the foundations for Redux...
+  */
 
 class Mortgage extends React.Component {
   state = {
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR'
+    amount: 200000,
+    term: 30,
+    apr: 5
   };
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.setState({ payment: this.calculatePayment(this.state) });
+  }
+
   handleChange = name => event => {
-    this.setState({
+    let state = Object.assign({}, this.state, {
       [name]: event.target.value
     });
+
+    state.payment = this.calculatePayment(state);
+
+    this.setState(state);
+  };
+
+  calculatePayment = state => {
+    let { amount, apr, term, payment } = state;
+
+    apr /= 1200;
+    term *= 12;
+    payment =
+      (amount * (apr * Math.pow(1 + apr, term))) /
+      (Math.pow(1 + apr, term) - 1);
+    payment = '$' + payment.toFixed(2);
+
+    return payment;
   };
 
   render() {
@@ -26,25 +71,40 @@ class Mortgage extends React.Component {
 
         <form className={classes.container} noValidate autoComplete="off">
           <TextField
-            id="name"
-            label="Name"
+            required
+            id="amount"
+            label="amount"
             className={classes.textField}
-            value={this.state.name}
-            onChange={this.handleChange('name')}
+            value={this.state.amount}
+            defaultValue="200000"
+            onChange={this.handleChange('amount')}
             margin="normal"
           />
           <TextField
-            id="uncontrolled"
-            label="Uncontrolled"
-            defaultValue="foo"
+            required
+            id="apr"
+            label="apr"
+            onChange={this.handleChange('apr')}
+            value={this.state.apr}
+            defaultValue="5"
             className={classes.textField}
             margin="normal"
           />
           <TextField
             required
-            id="required"
-            label="Required"
-            defaultValue="Hello World"
+            id="term"
+            label="term"
+            onChange={this.handleChange('term')}
+            value={this.state.term}
+            defaultValue="30"
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            id="payment"
+            label="payment"
+            value={this.state.payment}
             className={classes.textField}
             margin="normal"
           />
